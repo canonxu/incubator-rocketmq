4e385ae82e0057e7890b2ec74cfabd42e9f33802
Jianeng Xu <jianeng.xjn@cainiao.com>
2017-07-31 02:10:26 +0800

    测试quickstart consumer/producer ok

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -35,7 +35,10 @@ public class Consumer {
         /*
          * Instantiate with specified consumer group name.
          */
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_4");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("JIANENG_ROCKET_GROUP");
+
+        consumer.setNamesrvAddr("192.168.1.107:9876");
+
 
         /*
          * Specify name server addresses.
@@ -57,7 +60,7 @@ public class Consumer {
         /*
          * Subscribe one more more topics to consume.
          */
-        consumer.subscribe("TopicTest", "*");
+        consumer.subscribe("TopicTest-JIANENG", "*");
 
         /*
          *  Register callback to execute on arrival of messages fetched from brokers.
